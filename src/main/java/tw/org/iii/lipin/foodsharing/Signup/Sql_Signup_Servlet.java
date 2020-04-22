300d7294b6c43c9a115df2eb749f6facd9d80e61
xiaolipin <a0979225002@gmail.com>
2020-04-22 23:20:23 +0800

    更新後端無法儲存時間bug

--- src/main/java/tw/org/iii/lipin/foodsharing/Signup/Sql_Signup_Servlet.java
+++ src/main/java/tw/org/iii/lipin/foodsharing/Signup/Sql_Signup_Servlet.java
@@ -22,7 +22,6 @@ public class Sql_Signup_Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
         PrintWriter out = response.getWriter();
-        Date date = null;
 
         String account = request.getParameter("account");
         String password = request.getParameter("password");
@@ -30,38 +29,15 @@ public class Sql_Signup_Servlet extends HttpServlet {
         String createtime = request.getParameter("createTime");
         String md5passwd = MD5Utils.md5(password,MD5Utils.md5key);
 
-        try {
-            date =  getDataTime(createtime);
-        } catch (ParseException e) {
-            out.println(e.toString());
-        }
-
-
         String sql = "insert into user(account,password,phone,createtime)" +
                         "value (?,?,?,?)";
 
-        int count =  template.update(sql,account,md5passwd,phone,date);
+        int count =  template.update(sql,account,md5passwd,phone,createtime);
 
         out.println(count);
 
-        System.out.println(createtime +":aaaaaa");
 
-    }
 
-    /**
-     * 將日期轉sql日期存取格式
-     * @param createtime
-     * @return
-     * @throws ParseException
-     */
-    private Date getDataTime(String createtime) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        //轉java date格式
-        java.util.Date javaDate = simpleDateFormat.parse(createtime);
-        //轉 sql格式
-        Date sqlDate = new Date(javaDate.getTime());
-
-        return sqlDate;
     }
 
     @Override
