ea30de4b6fdd333a71e2217e03bcc994f3e79910
xiaolipin <a0979225002@gmail.com>
2020-05-22 03:02:45 +0800

    修改搜尋bug

--- src/main/java/tw/org/iii/lipin/foodsharing/HomeFragment/Search.java
+++ src/main/java/tw/org/iii/lipin/foodsharing/HomeFragment/Search.java
@@ -24,56 +24,44 @@ public class Search extends HttpServlet {
         JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
         String sql = null;
         JSONArray array = new JSONArray();
-        List<Map<String,Object>> maps = null;
-        System.out.println(searching+userid);
+        List<Map<String, Object>> maps = null;
+        System.out.println(searching + userid);
         try {
-            // 判斷兩次如果name搜尋是null就進入tag搜尋,反之如果name搜尋就有東西就執行name搜尋而已
-            sql = "select fc.*,us.account,us.img,us.username" +
-                    " from foodcard as fc " +
-                    " left join user_has_foodcards as uhf " +
+            sql = "select fc.*,us.account,us.img,us.username,us.token" +
+                    " from foodcard as fc" +
+                    " left join user_has_foodcards as uhf" +
                     " on fc.id = uhf.foodcard_id" +
                     " left join user as us" +
                     " on uhf.user_id = us.id" +
                     " where not uhf.user_id = ?" +
                     " and fc.status = ?" +
-                    " and fc.name like '%"+searching+"%'" +
-                    " order by fc.id desc ";
+                    " and fc.name like '%" + searching + "%'" +
+                    " union" +
+                    " select  fc.*,us.account,us.img,us.username,us.token" +
+                    " from foodcard as fc" +
+                    " left join user_has_foodcards as uhf" +
+                    " on fc.id = uhf.foodcard_id" +
+                    " left join user as us" +
+                    " on uhf.user_id = us.id" +
+                    " where not uhf.user_id = 60" +
+                    " and fc.status = 1" +
+                    " and fc.tag like '%" + searching + "%'" +
+                    " order by id desc";
 
-             maps =  template.queryForList(sql,userid,1);
-                System.out.println(maps.size());
-            if (maps == null || maps.isEmpty()){
+            maps = template.queryForList(sql, userid, 1);
+            System.out.println(maps.size());
 
-                sql = "select fc.*,us.account,us.img,us.username" +
-                        " from foodcard as fc " +
-                        " left join user_has_foodcards as uhf " +
-                        " on fc.id = uhf.foodcard_id" +
-                        " left join user as us" +
-                        " on uhf.user_id = us.id" +
-                        " where not uhf.user_id = ?" +
-                        " and fc.status = ?" +
-                        " and fc.tag like '%"+searching+"%'" +
-                        " order by fc.id desc ";
-                maps =  template.queryForList(sql,userid,1);
+            for (Map<String, Object> map : maps) {
 
-                for (Map<String,Object> map:maps){
+                array.put(map);
 
-                    array.put(map);
-                    out.println(array.toString());
-                    System.out.println(array.length());
-                }
-            }else if (maps != null || !maps.isEmpty()){
-
-                for (Map<String,Object> map:maps){
-                    array.put(map);
-                }
-                out.println(array.toString());
-                System.out.println(array.length());
             }
+            out.println(array.toString());
+            System.out.println(array.length());
         }catch (Exception e){
-            System.out.println(e.toString());
+            System.out.println("Search:"+e.toString());
         }
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
     }
